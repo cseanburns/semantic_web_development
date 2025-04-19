e29aac317edb8f95cdf566a3159efaeb699772b3
C. Sean Burns <sean.csb@gmail.com>
2025-04-19 11:49:17 -0400

    added file with pandoc command to convert md to epub

---
+++ pandoc2epub.sh
@@ -0,0 +1,34 @@
+# to generate an epub from this, see
+# https://pandoc.org/epub.html
+# add css file ; see link above
+
+#pandoc -o srb.epub title.txt \
+#    p1-introduction-to-semantic-web-development.md \
+#    1a-the-semantic-web.md \
+#    1b-user-experience-accessibility.md \
+#    1c-tools-setup-workflow.md \
+#    1d-designing-with-inkscape.md \
+#    p2-html5-and-the-web.md \
+#    2a-document-structure-and-metadata.md \
+#    2b-section-elements.md \
+#    2b1-using-vscode-and-github.md \
+#    2c-grouping-semantic-elements.md \
+#    2d-links-and-attributes.md \
+#    2e-embedded-content.md \
+#    2f-tables-and-forms.md \
+#    p3-css3-and-web-design.md \
+#    3a-css-getting-started.md \
+#    3a1-syntax-and-selectors.md \
+#    3a2-colors-units-measurements.md \
+#    3b-box-model.md \
+#    3b1-margins-borders-padding-sizing.md \
+#    3c-typography-and-font-styles.md \
+#    3d-images-lists-backgrounds.md \
+#    3e-responsive-layouts-with-flexbox.md \
+#    3e1-flexbox-basics.md \
+#    p4-document-metadata-json-ld-schema-org.md \
+#    4a-json-basics.md \
+#    4b-schema-org-schema-types-structured-data.md \
+#    4c-json-ld-and-schema-org-integration.md \
+#    p5-conclusion.md \
+#    a1-final-project.md 
